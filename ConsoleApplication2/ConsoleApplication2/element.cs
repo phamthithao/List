a6a9f1d2ded604f08c29b86d7948ded743a8ad4f
phamthithao <bunney322@gmail.com>
2016-09-26 08:31:17 +0700

    4455

---
+++ ConsoleApplication2/ConsoleApplication2/element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class element <T>
+    {
+        private T data;
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+
+       
+    }
+}
